458e269f6c611e682fd50cf6d3f0357bdbaee6c9
Nikita Chetverikov <n.chetverikov@ylab.io>
2022-01-22 16:24:17 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useRef, useState} from "react";
+import ReactDOM from "react-dom";
+import {
+    titleChanged,
+    taskDeleted,
+    completeTask,
+    loadTasks,
+    getTasks,
+    getTasksLoadingStatus, createTask,
+} from "./store/task";
+import configureStore from "./store/store";
+import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
+
+const store = configureStore();
+
+const App = (params) => {
+    const state = useSelector(getTasks());
+    const isLoading = useSelector(getTasksLoadingStatus());
+    const error = useSelector(getError());
+    const dispatch = useDispatch();
+    const inputTask = useRef('');
+
+    useEffect(() => {
+        dispatch(loadTasks());
+    }, []);
+
+    const handleCreateTask = () => {
+        const data = {title: inputTask.current.value, completed: false}
+        dispatch(createTask(data))
+    }
+    const changeTitle = (taskId) => {
+        dispatch(titleChanged(taskId));
+    };
+    const deleteTask = (taskId) => {
+        dispatch(taskDeleted(taskId));
+    };
+    if (isLoading) {
+        return <h1>Loading</h1>;
+    }
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return (
+        <>
+            <h1> App</h1>
+            <h3>Название таска</h3>
+            <input ref={inputTask}/>
+            <button onClick={handleCreateTask}>Создат задачу</button>
+            <ul>
+                {state.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.title}</p>
+                        <p> {`Completed: ${el.completed}`}</p>
+                        <button onClick={() => dispatch(completeTask(el.id))}>
+                            Complete
+                        </button>
+                        <button onClick={() => changeTitle(el.id)}>
+                            Change title
+                        </button>
+                        <button onClick={() => deleteTask(el.id)}>
+                            Delete
+                        </button>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
